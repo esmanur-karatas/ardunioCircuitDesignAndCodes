e62099a2114a2872505e3841ebc7abd4f91fe771
Esmanur Karatas <83882274+esmanur-karatas@users.noreply.github.com>
2024-01-23 10:05:46 +0300

    Add files via upload

---
+++ cakarLedFlipFlop/cakarLedFlipFlop.ino
@@ -0,0 +1,13 @@
+void setup() {
+  pinMode(10,OUTPUT);
+  pinMode(9,OUTPUT);
+}
+
+void loop(){
+  digitalWrite(10,HIGH);
+  digitalWrite(9,LOW);
+  delay(1000);
+  digitalWrite(10,LOW);
+  digitalWrite(9,HIGH);
+  delay(1000);
+}
